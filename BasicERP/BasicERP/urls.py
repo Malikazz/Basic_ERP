e47b45400c047e91ceb5834aebc20fc8fcde3a42
Paul Mana <paul@malikaz.com>
2022-01-24 11:24:22 -0700

    project rename

--- BasicERP/BasicERP/urls.py
+++ BasicERP/BasicERP/urls.py
@@ -1,4 +1,4 @@
-"""ChubbyBuffalo URL Configuration
+"""BasicERP URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('dashboard/', include('dashboard.urls'))
 ]
