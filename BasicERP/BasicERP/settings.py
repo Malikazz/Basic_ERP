9a086405413ac017d093debefffc51502bfa664d
Paul Mana <paul@malikaz.com>
2022-01-24 15:17:42 -0700

    crispy forms

--- BasicERP/BasicERP/settings.py
+++ BasicERP/BasicERP/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "users",
     "dashboard",
+    "crispy_forms",
 ]
 
 MIDDLEWARE = [
@@ -135,3 +136,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
