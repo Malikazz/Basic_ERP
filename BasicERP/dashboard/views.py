c78e673106001285046821d4ef19d64e3f9c64b5
Paul Mana <paul@malikaz.com>
2022-01-24 14:12:06 -0700

    templates and static dirs

--- BasicERP/dashboard/views.py
+++ BasicERP/dashboard/views.py
@@ -1,6 +1,9 @@
+from email import message
 from django.shortcuts import render
+from django.contrib import messages
 
 
 # Create your views here.
 def index(request):
+    messages.add_message(request, messages.INFO, "Hello world.")
     return render(request, "dashboard/index.html")
--- BasicERP/users/urls.py
+++ BasicERP/users/urls.py
@@ -1,3 +1,5 @@
 from django.urls import path, include
+from django.contrib.auth import urls
 
-urlpatterns = []
+app_name = "users"
+urlpatterns = [path("", include("django.contrib.auth.urls"))]
