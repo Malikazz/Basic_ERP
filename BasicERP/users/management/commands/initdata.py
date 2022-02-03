087ca5d4c02674b59489b77dc3769b4533f3748b
Paul Mana <paul@malikaz.com>
2022-02-02 17:50:40 -0700

    files moved around to make django happy, fixtures have base users and base groups now.

--- BasicERP/users/management/commands/initdata.py
+++ BasicERP/users/management/commands/initdata.py
@@ -8,7 +8,7 @@ class Command(BaseCommand):
     help = "DEV COMMAND: Fill databasse with a set of data for testing purposes"
 
     def handle(self, *args, **options):
-        call_command("loaddata", "initial_data")
+        call_command("loaddata", "dev_data")
         # Fix the passwords of fixtures
         for user in User.objects.all():
             user.set_password(user.password)
