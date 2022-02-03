83fb370fafa829f118a49de82780fb44ec0853aa
Paul Mana <paul@malikaz.com>
2022-02-03 12:07:25 -0700

    change default redirect for new path

--- BasicERP/BasicERP/settings.py
+++ BasicERP/BasicERP/settings.py
@@ -140,7 +140,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 # Default login redirect if no other if provided
-LOGIN_REDIRECT_URL = "/dashboard/"
+LOGIN_REDIRECT_URL = "/"
 
 LOGOUT_REDIRECT_URL = "/accounts/login/"
 
