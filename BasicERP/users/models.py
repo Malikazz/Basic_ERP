ce9b1bd13e5fb3d642bc3b5870349dfce812aeff
Paul Mana <paul@malikaz.com>
2022-02-02 17:37:53 -0700

    super basic order setup

--- BasicERP/users/models.py
+++ BasicERP/users/models.py
@@ -2,9 +2,3 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-
-
-class UserInfo(User):
-    """Used to store addtional info on the users"""
-
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
