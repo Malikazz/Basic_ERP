2decb9d589e1efd477f29fa29cf01e8210ac0897
Paul Mana <paul@malikaz.com>
2022-02-04 09:27:20 -0700

    urls for change / reset password

--- BasicERP/users/urls.py
+++ BasicERP/users/urls.py
@@ -3,4 +3,12 @@ from django.contrib.auth import urls
 from . import views
 
 app_name = "users"
-urlpatterns = []
+urlpatterns = [
+    path("change_password", views.user_change_password, name="user_change_password"),
+    path(
+        "reset_password",
+        views.user_reset_password,
+        name="user_reset_password",
+    ),
+    path("reset_password/<uid>/<token>/", views.user_reset, name="reset"),
+]
