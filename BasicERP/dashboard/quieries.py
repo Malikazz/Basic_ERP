5a4bd819ca90f48d5c764c4aad4cda4af2eee4c9
Paul Mana <paul@malikaz.com>
2022-02-09 13:37:53 -0700

    created function to handle like code, all roles now work

--- BasicERP/dashboard/quieries.py
+++ BasicERP/dashboard/quieries.py
@@ -3,63 +3,42 @@ from .models import Order, OrderDocument, OrderTag, OrderImage
 from django.contrib.auth.models import User
 from django.db.models import Q
 
-##TODO: can currently return duplicates looking into how to prevent this
-def get_orders_by_user_role(user: User) -> List[Dict[str, object]]:
-    orders = []
+
+def _get_order_and_related_data(user: User, orders: dict) -> None:
+
+    order_list = list(
+        Order.objects.filter(~Q(order_tags__name="Archive"))
+        .prefetch_related("order_documents")
+        .prefetch_related("order_images")
+        .prefetch_related("order_tags")
+    )
+    for item in order_list:
+        order_documents = list(item.order_documents.all())
+        order_images = list(item.order_images.all())
+        order_tags = list(item.order_tags.all())
+        orders["order_" + str(item.id)] = {
+            "order": item,
+            "order_documents": order_documents,
+            "order_image": order_images,
+            "order_tags": order_tags,
+        }
+
+
+def get_orders_by_user_role(user: User) -> Dict[str, Dict[str, object]]:
+    orders = {}
     user_groups = get_all_users_groups(user)
     if "Managing Director" in user_groups:
-
-        order_list = list(
-            Order.objects.filter(~Q(order_tags__name="Archive"))
-            .prefetch_related("order_documents")
-            .prefetch_related("order_images")
-            .prefetch_related("order_tags")
-        )
-        for item in order_list:
-            order_documents = list(item.order_documents.all())
-            order_images = list(item.order_images.all())
-            orders.append(
-                {
-                    "order": item,
-                    "order_documents": order_documents,
-                    "order_image": order_images,
-                }
-            )
+        _get_order_and_related_data(user, orders)
     if "Inventory Manager" in user_groups:
-        orders.append(
-            Order.objects.filter(order_tags__name="Inventory")
-            .values_list()
-            .prefetch_related("order_documents")
-            .prefetch_related("order_tags")
-        )
+        _get_order_and_related_data(user, orders)
     if "Production Manager" in user_groups:
-        orders.append(
-            Order.objects.filter(order_tags__name="Production Review")
-            .values_list()
-            .prefetch_related("order_documents")
-            .prefetch_related("order_tags")
-        )
+        _get_order_and_related_data(user, orders)
     if "Sales" in user_groups:
-        orders.append(
-            Order.objects.filter(order_tags__name="Sales")
-            .values_list()
-            .prefetch_related("order_documents")
-            .prefetch_related("order_tags")
-        )
+        _get_order_and_related_data(user, orders)
     if "Production Staff" in user_groups:
-        orders.append(
-            Order.objects.filter(order_tags__name="Production")
-            .values_list()
-            .prefetch_related("order_documents")
-            .prefetch_related("order_tags")
-        )
+        _get_order_and_related_data(user, orders)
     if "Designer" in user_groups:
-        orders.append(
-            Order.objects.filter(order_tags__name="Design")
-            .values_list()
-            .prefetch_related("order_documents")
-            .prefetch_related("order_tags")
-        )
+        _get_order_and_related_data(user, orders)
 
     return orders
 
